60add4fb9a6933af29722c20345fc9e8fb435fef
Willy Chyr <wcchyr@gmail.com>
2011-07-14 18:06:04 -0400

    added Distorted Circle example - still incomplete

---
+++ DistortedCircle/src/ParticleController.cpp
@@ -0,0 +1,70 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/Rand.h"
+#include "cinder/Vector.h"
+#include "ParticleController.h"
+
+using namespace ci;
+using std::list;
+
+ParticleController::ParticleController()
+{
+    angle = 25;
+}
+
+void ParticleController::update( const Perlin &perlin, const Channel32f &channel, const Vec2i &mouseLoc )
+{
+	for( list<Particle>::iterator p = mParticles.begin(); p != mParticles.end(); ) {
+		if( p->mIsDead ){
+			p = mParticles.erase( p );
+		} else {
+			p->update( perlin, channel, mouseLoc );
+			++p;
+		}
+	}
+}
+
+void ParticleController::draw()
+{
+	for( list<Particle>::iterator p = mParticles.begin(); p != mParticles.end(); ++p ){
+		p->draw();
+	}
+    
+    if (coordinates.size() > 1) {
+        for (int i = 0; i < coordinates.size()-2; i++ ) {
+            gl::drawLine(coordinates[i], coordinates[i+1]);
+        }
+    }
+}
+
+void ParticleController::addParticles( int amt, const Vec2i &mouseLoc, const Vec2f &mouseVel )
+{
+	for( int i=0; i<amt; i++ )
+	{
+		angle = i * ( (2 * M_PI)/amt);
+        
+        //Vec2f loc = mouseLoc + Rand::randVec2f() * 5.0f;
+		Vec2f loc;
+        loc.x = mouseLoc.x + 50*sin(angle);
+        loc.y = mouseLoc.y + 50*cos(angle);
+        
+        coordinates.push_back(loc);
+        
+        //Vec2f velOffset = Rand::randVec2f() * Rand::randFloat( 1.0f, 5.0f );
+		//Vec2f vel = mouseVel * 0.375f + velOffset;
+        
+        Vec2f vel;
+        vel.set( sin(angle), cos(angle) );
+        vel *= Rand::randFloat(3, 10);
+        
+		mParticles.push_back( Particle( loc, vel ) );
+	}
+}
+
+void ParticleController::removeParticles( int amt )
+{
+	for( int i=0; i<amt; i++ )
+	{
+		mParticles.pop_back();
+	}
+}
+
