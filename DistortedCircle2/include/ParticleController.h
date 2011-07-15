e7a7fea087db1d9734456d7da6dd857e3aaead0b
Willy Chyr <wcchyr@gmail.com>
2011-07-15 15:00:08 -0400

    made changes to DistortedCircle2

---
+++ DistortedCircle2/include/ParticleController.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "cinder/Channel.h"
+#include "Particle.h"
+#include "cinder/Perlin.h"
+#include <list>
+#include <vector>
+
+class ParticleController {
+ public:
+	ParticleController();
+	void update( const ci::Perlin &perlin, const ci::Channel32f &channel, const ci::Vec2i &mouseLoc );
+	void draw();
+	void addParticles( int amt, const ci::Vec2i &mouseLoc, const ci::Vec2f &mouseVel );
+	void removeParticles( int amt );
+	
+	std::vector<Particle>	mParticles;
+    
+    std::vector<ci::Vec2f> coordinates;
+    
+    float angle;
+};
