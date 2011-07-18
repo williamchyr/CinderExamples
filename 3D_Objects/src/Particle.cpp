06971536f56e1cbd0da3276f82f7987358b9853a
Willy Chyr <wcchyr@gmail.com>
2011-07-18 12:59:10 -0400

    added 3D Objects

---
+++ 3D_Objects/src/Particle.cpp
@@ -0,0 +1,88 @@
+#include "Particle.h"
+#include "cinder/Rand.h"
+#include "cinder/gl/gl.h"
+#include "cinder/app/AppBasic.h"
+
+using namespace ci;
+
+Particle::Particle()
+{
+}
+
+Particle::Particle( Vec2f loc, Vec2f vel )
+{
+	mLoc			= loc;
+	mVel			= vel*0.1;
+    startLoc        = loc;
+    
+    mLoc += mVel*Rand::randInt( 1, 20 );
+
+	mDecay			= Rand::randFloat( 0.96f, 0.99f );
+	mRadius			= 0.0f;
+	mScale			= 3.0f;
+	mAge			= 0;
+	mLifespan		= 200;
+	mAgePer			= 1.0f;
+	mIsDead			= false;
+    
+    angle1 = 0;
+    particleAngle = 0;
+    angleChange = Rand::randFloat(30, 400);
+}	
+
+
+void Particle::update(const Perlin &perlin)
+{	
+	
+  angle1 += M_PI/25;
+    
+    // get Perlin noise value based on my location and
+	// elapsed seconds and turn this noise value into an angle.
+	
+    /*
+    float nX = mLoc.x * 0.5f;
+	float nY = mLoc.y * 0.5f;
+	float nZ = app::getElapsedSeconds() * 0.1f;
+	float noise = perlin.fBm( nX, nY, nZ);
+	float angle = noise * 15.0f;
+     */
+    /*
+	Vec2f noiseVector( cos( angle ), sin( angle ) );
+
+	mVel += noiseVector * 0.2f * ( 1.0f - mAgePer );
+    */
+    
+   // particleAngle += M_PI/angleChange;
+    
+    
+    
+    mLoc += mVel * sin(particleAngle);
+    
+   // mLoc.x = startLoc.x + 120*sin(angle1);
+   // mLoc.y = startLoc.y + 120*cos(angle1);
+	//mLoc += mVel;
+	//mVel *= mDecay;
+
+	mRadius	= mScale * mAgePer;
+	
+	//mAge++;
+	if( mAge > mLifespan ) mIsDead = true;
+	mAgePer = 1.0f - ( (float)mAge/(float)mLifespan );
+}
+
+void Particle::draw()
+{
+	gl::drawSolidCircle( mLoc, mRadius * mAgePer );
+    
+	/*
+    coordinates.push_back(mLoc);
+    
+    for (int i = 0; i < coordinates.size()-1; i++) {
+        gl::drawLine( coordinates[i], coordinates[i+1] );
+    }
+	 */
+	
+}
+
+
+
---
+++ 3D_Objects/src/ParticleController.cpp
@@ -0,0 +1,101 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/Rand.h"
+#include "cinder/Vector.h"
+#include "ParticleController.h"
+#include <vector>
+
+using namespace ci;
+using std::list;
+using std::vector;
+
+ParticleController::ParticleController()
+{
+    angle = 25;
+}
+
+void ParticleController::init() {
+
+    dead = false;
+    
+}
+
+void ParticleController::update( const Perlin &perlin, const Vec2i &mouseLoc )
+{
+
+	//coordinates.clear();
+    
+    for (int i = 0; i < mParticles.size(); i++ ) {
+        
+        float angle;
+        mParticles[i].particleAngle += M_PI/mParticles[i].angleChange;
+		mParticles[i].update(perlin);
+		
+		coordinates[i] = mParticles[i].mLoc;
+		if (mParticles[i].mIsDead){
+            dead = true;
+			//mParticles.erase (mParticles.begin()+i);
+        }
+	}
+    
+}
+
+void ParticleController::draw()
+{
+	/*
+	 for( list<Particle>::iterator p = mParticles.begin(); p != mParticles.end(); ++p ){
+		p->draw();
+	}
+	 */
+	
+    glColor4f( 1.0, 1.0, 0.0, 1.0*mParticles[0].mAgePer );
+    
+	for (int i = 0; i < mParticles.size(); i++ ) {
+		mParticles[i].draw();
+	}
+    
+    
+    if (coordinates.size() > 1) {
+        for (int i = 0; i < coordinates.size()-1; i++ ) {
+        
+            gl::drawLine(coordinates[i], coordinates[i+1]);
+           
+        }
+        
+        gl::drawLine( coordinates[0], coordinates[ coordinates.size()-1 ] );
+    }
+    
+    
+}
+
+void ParticleController::addParticles( int amt, const Vec2i &mouseLoc, const Vec2f &mouseVel )
+{
+	for( int i=0; i<amt; i++ )
+	{
+		angle = i * ( (2 * M_PI)/amt);
+        
+        //Vec2f loc = mouseLoc + Rand::randVec2f() * 5.0f;
+		Vec2f loc;
+        loc.x = mouseLoc.x + 100*sin(angle);
+        loc.y = mouseLoc.y + 100*cos(angle);
+        
+        coordinates.push_back(loc);
+        
+        //Vec2f velOffset = Rand::randVec2f() * Rand::randFloat( 1.0f, 5.0f );
+		//Vec2f vel = mouseVel * 0.375f + velOffset;
+        
+        Vec2f vel;
+        vel.set( sin(angle), cos(angle) );
+        vel *= Rand::randFloat(3, 10);
+        
+		mParticles.push_back( Particle( loc, vel ) );
+	}
+}
+
+void ParticleController::removeParticles( int amt )
+{
+	for( int i=0; i<amt; i++ )
+	{
+		mParticles.pop_back();
+	}
+}
+
